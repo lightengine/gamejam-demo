d4e1b6b2d7e7283e1d2641f5caf7629aa5f60a91
Brandon Thomas <bt@brand.io>
2013-09-20 23:53:33 -0400

    Init

---
+++ circle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from lib import dac
+from lib.common import *
+
+import math
+import itertools
+import sys
+
+LASER_POWER_DENOM = 1.0
+MAXRAD = 10260
+USERAD = int(MAXRAD/10)
+SAMPLE_PTS = 100 # 30 and below very damaging to galvos
+
+
+class CirclePointStream(object):
+
+	def produce(self):
+
+		RESIZE_SPEED_INV = 200
+		while True:
+			rad = int(USERAD)
+			for i in xrange(0, SAMPLE_PTS, 1):
+				i = float(i) / SAMPLE_PTS * 2 * math.pi
+				x = int(math.cos(i) * rad)
+				y = int(math.sin(i) * rad)
+				yield (x, y, CMAX/LASER_POWER_DENOM,
+							CMAX/LASER_POWER_DENOM,
+							CMAX/LASER_POWER_DENOM)
+
+	def __init__(self):
+		self.called = False
+		self.stream = self.produce()
+
+	def read(self, n):
+		d = [self.stream.next() for i in xrange(n)]
+		return d
+
+while True:
+	try:
+		d = dac.DAC(dac.find_first_dac())
+		ps = CirclePointStream()
+		d.play_stream(ps)
+	except KeyboardInterrupt:
+		sys.exit()
+	except Exception as e:
+		print e
+		continue
