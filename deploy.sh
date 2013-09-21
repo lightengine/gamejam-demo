403e6be440c0fae515ee2c170a9be5fbd3ae8ed5
Brandon Thomas <bt@brand.io>
2013-09-20 23:56:05 -0400

    Deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash 
+# Brandon Thomas <bt@brand.io>
+# Sync to the remote server
+
+# Sets the directory of the current script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJ_DIR="$DIR/.."
+
+# Password is not set! Use `ssh-copy-id` to prevent fumbling around with it.
+export USER='laser'
+export HOST='169.254.10.49'
+export DIR_ROOT='/home/laser/dev/laser/gamejam-demo'
+
+#
+# Functions and Aliases
+# _____________________
+
+# XXX: May need to `sudo visudo` and comment out `Defaults requiretty`
+shopt -s expand_aliases
+alias rsync="rsync -zrpvuL -e ssh --exclude-from=$DIR/exclude.txt --progress" 
+
+function print_status() {
+	status=$1
+	length=${#1}
+	echo ''
+	echo $status
+	for i in $(seq $length); do echo -n '='; done
+	echo ''
+}
+
+function notify_complete() {
+	message=$1
+	notify-send -i /usr/share/icons/gnome/32x32/emotes/face-laugh.png \
+				-t 700 \
+				"Deploy" "$message"
+}
+
+##
+### Actual Deploy Scripting...
+####
+
+echo "Uploading to $USER@$HOST:$DIR_ROOT..."
+
+print_status '[rsync] All'
+rsync .  $USER@$HOST:$DIR_ROOT
+
+print_status 'Play script'
+ssh -n -f $USER@$HOST "sh -c 'python $DIR_ROOT/circle.py'"
+
