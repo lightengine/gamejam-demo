3711a4076189b93d7f55990e871871001a93fd61
Brandon Thomas <bt@brand.io>
2013-09-21 21:28:47 -0400

    position and region

--- lib/region.py
+++ lib/region.py
@@ -1,19 +1,27 @@
+from position import Position
 
 class Region(object):
 
 	def __init__(self, centerX=0, centerY=0, width=100, height=100,
 						centerPosition=None):
 
-		self.center = None
-		self.width = None
-		self.height = None
+		self._center = None
+		self._width = width
+		self._height = height
 
 		if centerPosition:
-			self.center = centerPosition
+			self._center = centerPosition
 		else:
-			self.center = Position(centerX, centerY)
+			self._center = Position(centerX, centerY)
 
-		self.topX = self.center.x
+		self._top = self._center.x + height/2
+		self._bottom = self._center.x - height/2
+		self._left = self._center.y - width/2
+		self._right = self._center.y + width/2
 
-	def isWithin(position):
+	def contains(self, position):
+		if position.x > self._left and position.x < self._right and \
+			position.y > self._bottom and position.y < self._top:
+				return True
+		return False
 
